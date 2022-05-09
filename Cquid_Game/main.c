170c8cf9544687a881b481d82b4c66a166457ad7
[haezzang] <songna89@naver.com>
2022-05-10 08:48:12 +0900

    Cquid Game 메인 프로토

---
+++ Cquid_Game/main.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+//타이틀 찍기
+void title() {
+	gotoxy(35, 4); printf(" _____ ____  __    __ _____ _____   ");
+	gotoxy(35, 5); printf("/ ____/ __ ＼| |  | ||_   _|  __ ＼ ");
+	gotoxy(35, 6); printf("| |   | |  | | |  | |  | | | |  | |");
+	gotoxy(35, 7); printf("| |   | |  | | |  | |  | | | |  | | ");
+	gotoxy(35, 8); printf("| |___| |__| | |__| | _| |_| |__| | ");
+	gotoxy(35, 9); printf("＼____＼___＼＼____/ |_____|_____/  ");
+
+	gotoxy(35, 10); printf("   _____          __    __ ______ ");
+	gotoxy(35, 11); printf("  / ____|   /＼   |  ＼/  |  ____|");
+	gotoxy(35, 12); printf(" | |  __   /  ＼  | ＼  / | |__ ");
+	gotoxy(35, 13); printf(" | | |_ | / /＼＼ | |＼/| |  __|");
+	gotoxy(35, 14); printf(" | |__| |/ ____ ＼| |   | | |____ ");
+	gotoxy(35, 15); printf("  ＼____/_/   ＼_＼_|   |_|______|");
+}
+
+//게임 포커스
+void focus() {
+	gotoxy(11, 20);
+	printf("┌─────────────────┐\n");
+	gotoxy(11, 21);
+	printf("│                 │\n");
+	gotoxy(11, 22);
+	printf("│                 │\n");
+	gotoxy(11, 23);
+	printf("│                 │\n");
+	gotoxy(11, 24);
+	printf("└─────────────────┘\n");
+}
+
+//게임선택
+void select() {
+
+	int chk=0;
+	gotoxy(13, 18); printf("게임 선택 : \n");
+	gotoxy(20, 18); scanf_s("%d", & chk);
+
+	switch (chk)
+	{
+	case 1: main(); break;
+	case 2: break;
+	case 3: break;
+	case 4: break;
+	case 5: break;
+	case 6: break;
+
+	}
+}
+int main(void) {
+	title();
+	focus();
+
+	gotoxy(13, 22); printf("① 무궁화 꽃\n");
+	gotoxy(26, 22);  printf("② 달고나뽑기");
+	gotoxy(39, 22);  printf("③ 줄다리기");
+	gotoxy(13, 26);  printf("④ 구슬치기");
+	gotoxy(26, 26);  printf("⑤ 징검다리");
+	gotoxy(39, 26);  printf("⑥ 리듬게임");
+
+	select();
+}
---
+++ Cquid_Game/main.h
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<windows.h>
+
+//글자색 정의
+#define BLACK 0 
+#define BLUE 1 
+#define GREEN 2 
+#define CYAN 3 
+#define RED 4 
+#define MAGENTA 5 
+#define BROWN 6 
+#define LIGHTGRAY 7 
+#define DARKGRAY 8 
+#define LIGHTBLUE 9 
+#define LIGHTGREEN 10 
+#define LIGHTCYAN 11 
+#define LIGHTRED 12 
+#define LIGHTMAGENTA 13 
+#define YELLOW 14 
+#define WHITE 15 
+
+
+//좌표함수
+void gotoxy(int x, int y)
+{
+	COORD Pos;
+	Pos.X = 2 * x;
+	Pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+//글자색 
+void textcolor(int foreground, int background)
+{
+	int color = foreground + background * 16;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
